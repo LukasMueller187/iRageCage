d2a13b78a3ef5f5e06e09b42f8a806d7e74c9627
Adriano Lucieri <adriano.lucieri@outlook.com>
2020-04-04 23:52:00 +0200

    Add username requirement and Join Room Button

--- game/static/game/js/iRageCageReact/scenes/Home/components/Home.jsx
+++ game/static/game/js/iRageCageReact/scenes/Home/components/Home.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Redirect} from 'react-router';
 
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {'random': ''};
+        this.state = {
+            'random': '',
+            'username': '',
+            'redirect': false
+        };
+        this.onUsernameChange = this.onUsernameChange.bind(this);
+        this.joinRoom = this.joinRoom.bind(this);
     }
 
     componentDidMount() {
         this.setState({'random': parseInt(Math.random() * 10000)});
+        document.getElementById('username_input').focus();
+    }
+
+    onUsernameChange(event) {
+        event.preventDefault();
+        this.setState({'username': event.target.value});
+        document.getElementById('username_input').focus();
+    }
+
+    joinRoom(event) {
+        event.preventDefault();
+        if (this.state.username.length !== 0) {
+            this.setState(({'redirect': true}));
+        }
+        else {
+            this.setState(({'redirect': false}));
+            alert("Please specify a username!")
+        }
     }
 
     render() {
+        if (this.state.redirect) {
+            return <Redirect push to={"/play/#" + this.state.random} />;
+        }
         return (
             <div>
                 <p>Hi! Please join a room</p>
-                <Link to={`/play/#${this.state.random}`}>Go</Link>
+                <form>
+                    <input id="username_input" type="text" onChange={this.onUsernameChange}
+                           value={this.state.username}/>
+                    <button onClick={this.joinRoom} type="submit">Join Room</button>
+                </form>
             </div>
         );
     }
