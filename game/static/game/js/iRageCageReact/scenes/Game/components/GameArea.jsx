7476f3629e7ad66740c6aca367d6d6b5a4b58538
Adriano Lucieri <adriano.lucieri@outlook.com>
2020-05-31 23:59:56 +0200

    Added Matter.js as physics engine. Replaced SVGs by centered versions. Fixed mouse pointer alignment.

--- game/static/game/js/iRageCageReact/scenes/Game/components/GameArea.jsx
+++ game/static/game/js/iRageCageReact/scenes/Game/components/GameArea.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import decomp from 'poly-decomp';
+window.decomp = decomp;
+// https://github.com/liabru/matter-js/issues/559
+import Matter from 'matter-js';
+
+// module aliases
+let Engine = Matter.Engine,
+    World = Matter.World,
+    Body = Matter.Body,
+    Events = Matter.Events,
+    Bodies = Matter.Bodies,
+    Runner = Matter.Runner;
 
 function GameObject() {
     return {
@@ -36,18 +48,102 @@ export default class GameArea extends React.Component {
         this.logicChoosePower = this.logicChoosePower.bind(this);
         this.handleOnMouseDown = this.handleOnMouseDown.bind(this);
         this.drawBall = this.drawBall.bind(this);
+        this.Ball = this.Ball.bind(this);
+        this.checkWinCondition = this.checkWinCondition.bind(this);
+
+        this.ball_radius = 60;
+        this.cup_x = 400;
+        this.cup_width = 300;
+        this.cup_height = 420;
+        this.winState = 'None';
+
+        this.canvas_height = 1000;
+        this.canvas_width = 1920;
+        console.log(window.innerWidth)
+
+        this.engine = Engine.create({positionIteration:20});
+        this.ground_height = this.canvas_height;
+        this.ground_width = this.canvas_width;
+        this.ground_thickness = 100;
+        let world = this.engine.world;
+        this.ground = Bodies.rectangle(this.ground_width/2, this.ground_height+(this.ground_thickness/2), this.ground_width,
+            this.ground_thickness, {friction: 0.3, restitution:0, isStatic: true});
+        this.ball_physics = new this.Ball(960, 100, this.ball_radius);
+
+        let cupOffset = 30;
+
+        // TODO: Thin walls causes problems with simulation. Shape needs to be optimized as well.
+        // TODO: Align physics object with rendered image
+        let cup_vertices = [
+            {x: 0, y:0}, // Oben links außen
+            {x: 5, y:0}, // Oben links innen
+            {x: cupOffset+5, y: this.cup_height-5},
+            {x: this.cup_width-cupOffset-5, y: this.cup_height-5},
+            {x: this.cup_width, y: 0}, // Oben rechts innen
+            {x: this.cup_width+5, y: 0}, // Oben rechts außen
+            {x: this.cup_width-cupOffset, y: this.cup_height}, // Unten rechts
+            {x: cupOffset, y: this.cup_height} // Unten links
+        ];
+        let options_cup = {
+            friction: 0.3,
+            restitution: 0.2,
+            isStatic: true
+        };
+        this.cup_static  = Bodies.fromVertices(this.cup_x+(this.cup_width/2), this.ground_height-(this.cup_height/4), cup_vertices, options_cup);
+
+        let winZoneVertices = [
+            {x: cupOffset+2, y: 0},
+            {x: this.cup_width-cupOffset-2, y: 0},
+            {x: this.cup_width-cupOffset, y: this.cup_height/2},
+            {x: cupOffset, y: this.cup_height/2},
+        ];
+        let optionsWinZone = {
+            isSensor: true,
+            isStatic: true
+        };
+        this.winZone  = Bodies.fromVertices(this.cup_x+(this.cup_width/2), this.ground_height-(this.cup_height/4), winZoneVertices, optionsWinZone);
+
+        World.add(world, this.ball_physics.body);
+        World.add(world, this.ground);
+        World.add(world, this.cup_static);
+        World.add(world, this.winZone);
+
+        Events.on(this.engine, 'collisionStart', this.checkWinCondition);
+
+        Runner.run(this.engine);
+    }
+
+    checkWinCondition(event) {
+        let pairs = event.pairs;
+
+        for (let i = 0, j = pairs.length; i !== j; ++i) {
+            let pair = pairs[i];
+
+            if (pair.bodyA === this.ball_physics.body && pair.bodyB === this.winZone) {
+                this.winState = 'Winner!';
+                console.log(pair);
+            } else if (pair.bodyB === this.ball_physics.body && pair.bodyA === this.winZone) {
+                this.winState = 'Winner!';
+                console.log(pair);
+            }
+        }
+    }
+
+    Ball(x, y, radius) {
+        this.body = Bodies.circle(x, y, radius, {friction: 0.3, restitution: 0.99, isStatic: false, frictionAir:0.01, density:0.001});
     }
 
     componentDidMount() {
         this.context = this.canvas.current.getContext('2d');
+        //this.setState({'status': this.status.moving});
         this.setState({'status': this.status.choose_direction});
         addEventListener('mousedown', this.handleOnMouseDown);
         setInterval(this.update, 1 / 60 * 1000);
         addEventListener('mousemove', event => {
             const canvas_coords = this.canvas.current.getBoundingClientRect();
             this.setState({
-                'mouse_x': event.clientX - canvas_coords.left,
-                'mouse_y': event.clientY - canvas_coords.top,
+                'mouse_x': (window.innerWidth / (this.canvas_width*0.8)) * (event.clientX - canvas_coords.left) ,
+                'mouse_y': (window.innerHeight / (this.canvas_height*0.65)) * (event.clientY - canvas_coords.top) ,
             });
         });
     }
@@ -69,19 +165,21 @@ export default class GameArea extends React.Component {
     }
 
     logicChooseDirection() {
+        this.winState = 'None';
+
         let cup_front_bg = new Image();
         cup_front_bg.src = cup_front_bg_url;
         let cup_bg_object = this.state.cup_bg;
         cup_bg_object.update = () => {
             let context = this.canvas.current.getContext('2d');
-            context.drawImage(cup_front_bg, 66, 14, 240 - 66, 267 - 14, 500, 1000 - (267 - 14), 240 - 66, 267 - 14);
+            context.drawImage(cup_front_bg, 0, 0, 1000, 1400, this.cup_x, this.canvas_height - 420, 300, 420);
         };
         let cup_front_fg = new Image();
         cup_front_fg.src = cup_front_fg_url;
         let cup_fg_object = this.state.cup_fg;
         cup_fg_object.update = () => {
             let context = this.canvas.current.getContext('2d');
-            context.drawImage(cup_front_fg, 66, 14, 240 - 66, 267 - 14, 500, 1000 - (267 - 14), 240 - 66, 267 - 14);
+            context.drawImage(cup_front_fg, 0, 0, 1000 , 1400, this.cup_x, this.canvas_height - 420, 300, 420);
         };
 
         let gauge_object = this.state.gauge;
@@ -132,20 +230,23 @@ export default class GameArea extends React.Component {
     }
 
     logicMoving() {
+
+        // this.ball_physics.show();
         let gauge_object = this.state.gauge;
         gauge_object.update = () => {
         };
         let ball_object = this.state.ball;
         ball_object.x = this.state.mouse_x;
         ball_object.y = this.state.mouse_y;
-        ball_object.y_dot = 10 * gauge_object.power * Math.sin(gauge_object.angle / 180 * Math.PI);
-        ball_object.x_dot = 10 * -gauge_object.power * Math.cos(gauge_object.angle / 180 * Math.PI);
+        ball_object.y_dot = 0.5 * gauge_object.power * Math.sin(gauge_object.angle / 180 * Math.PI);
+        ball_object.x_dot = 0.5 * -gauge_object.power * Math.cos(gauge_object.angle / 180 * Math.PI);
+
+        Body.setPosition(this.ball_physics.body, {x:ball_object.x+60, y:ball_object.y+60});
+        Body.setVelocity(this.ball_physics.body, {x: ball_object.x_dot, y: ball_object.y_dot});
+
         ball_object.update = () => {
-            ball_object.y += 1 / 60 * ball_object.y_dot + 0.5 * (1 / 60) * (1 / 60) * 1200;
-            ball_object.y_dot += 1 / 60 * 1200;
-            ball_object.x += 1 / 60 * ball_object.x_dot;
-            if (ball_object.y > 1000 - 120)
-                ball_object.y_dot = -Math.abs(ball_object.y_dot * 0.8);
+            ball_object.y = this.ball_physics.body.position.y-60;
+            ball_object.x = this.ball_physics.body.position.x-60;
             this.drawBall(ball_object.x, ball_object.y);
         };
         this.setState({'ball': ball_object, 'gauge': gauge_object});
@@ -155,7 +256,7 @@ export default class GameArea extends React.Component {
         let ball = new Image();
         ball.src = ball_url;
         let context = this.canvas.current.getContext('2d');
-        context.drawImage(ball, 0, 0, 200, 200, x, y, 120, 120);
+        context.drawImage(ball, 0, 0, 200, 200, x, y, this.ball_radius*2, this.ball_radius*2);
     }
 
     handleOnMouseDown() {
@@ -173,20 +274,47 @@ export default class GameArea extends React.Component {
         }
     }
 
-
     update() {
-        this.canvas.current.getContext('2d').clearRect(0, 0, 1920, 1000);
+        console.log(this.winState);
+        this.canvas.current.getContext('2d').clearRect(0, 0, this.canvas_width, this.canvas_height);
+        // Draw ground
+        let context = this.canvas.current.getContext('2d');
+        // context.drawImage(ball, 0, 0, 200, 200, x, y, 120, 120);
+        context.beginPath();
+        context.lineWidth = "6";
+        context.strokeStyle = "black";
+        context.rect(0, this.ground_height, this.ground_width, this.ground_thickness);
+        context.stroke();
+
         this.state.cup_bg.update();
         this.state.ball.update();
         this.state.cup_fg.update();
         this.state.gauge.update();
+
+        // Draw cup physics model vertices
+        let cupOffset = 50;
+        let cupLidOffset = 25;
+        context.fillStyle = '#f00';
+        context.beginPath();
+        context.moveTo(this.cup_x, this.ground_height-this.cup_height);
+        context.lineTo(this.cup_x+cupLidOffset,this.ground_height-this.cup_height+cupLidOffset);
+        context.lineTo(this.cup_x+cupOffset+2,this.ground_height-this.cup_height+this.cup_height-2);
+        context.lineTo(this.cup_x+this.cup_width-cupOffset-2, this.ground_height-this.cup_height+this.cup_height-2);
+        context.lineTo(this.cup_x+this.cup_width-cupLidOffset,this.ground_height-this.cup_height+cupLidOffset);
+        context.lineTo(this.cup_x+this.cup_width, this.ground_height-this.cup_height);
+        context.lineTo(this.cup_x+this.cup_width-cupLidOffset+2,this.ground_height-this.cup_height+cupLidOffset);
+        context.lineTo(this.cup_x+this.cup_width-cupOffset, this.ground_height-this.cup_height+this.cup_height);
+        context.lineTo(this.cup_x+cupOffset, this.ground_height-this.cup_height+this.cup_height);
+        context.lineTo(this.cup_x+cupLidOffset-2,this.ground_height-this.cup_height+cupLidOffset);
+        context.closePath();
+        context.fill();
     }
 
     render() {
         return <>
             <div>
                 <canvas ref={this.canvas}
-                        height={1000} width={1920}/>
+                        height={this.canvas_height} width={this.canvas_width}/>
             </div>
         </>;
     }
